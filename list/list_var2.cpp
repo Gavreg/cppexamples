3ff0be2f2894673367ed73083cdbe0789cc10f90
gavreg <faktorial5@gmail.com>
2026-04-13 18:44:14 +0300

    list

--- list/list_var2.cpp
+++ list/list_var2.cpp
@@ -6,33 +6,33 @@
 
 #include <iostream>
 #include <stdexcept>
-#include <stdlib.h>
+#include <string>
 
+template <typename T> //или template<class T>
 struct Node_t
     {
-        int value;
+        T value;
 
         Node_t *next = nullptr;
 
         ~Node_t() { delete next; }
     };
 
+template <typename T> 
 class list_t
 {
 private:
-    Node_t *begin = nullptr;
-    Node_t *end = nullptr;
+    Node_t<T> *begin = nullptr;
+    Node_t<T> *end = nullptr;
 public:
-    
     ~list_t() {delete begin;}
 
-
-    Node_t* at (int num)
+    Node_t<T>* at (int num)
     {
        if (!begin)
         throw std::out_of_range("Index is out of range");
        
-        Node_t *cur_node = begin;
+        auto *cur_node = begin;
         int i = 0;
 
         while (i != num)
@@ -45,9 +45,9 @@ public:
         return cur_node;
     }
 
-    void push_back (int value)
+    void push_back (const T &value)
     {
-        Node_t *new_node = new Node_t;
+        auto *new_node = new Node_t<T>;
         new_node->value = value;
 
         if (begin == nullptr)
@@ -65,16 +65,28 @@ public:
 
 int main()
 {
-    list_t list;
+    list_t<int> list_int;
+    
+    list_int.push_back(1);
+    list_int.push_back(2);  
+    list_int.push_back(3);
 
-    list.push_back(1);
-    list.push_back(2);  
-    list.push_back(3);
-
-    for (Node_t *cur_node = list.at(0); cur_node ;
+    for (Node_t<int> *cur_node = list_int.at(0); cur_node ;
             cur_node=cur_node->next)
         std::cout << cur_node->value << " ";
 
+    std::cout<<std::endl;
+
+    list_t<std::string> list_str;
+    list_str.push_back("Stankin");
+    list_str.push_back("is the");
+    list_str.push_back("best");
+   
+    for (auto cur_node = list_str.at(0); cur_node ;
+            cur_node=cur_node->next)
+        std::cout << cur_node->value << " ";
+
+    std::cout<<std::endl;
     
     return 0;
 }
